7ec29033e137b3931e91cfaeebd1989246bcc1a4
Ivor Lewis <ivor.lewis@gmail.com>
2024-03-02 17:41:12 -0700

    svelte basics

---
+++ src/App.svelte
@@ -0,0 +1,64 @@
+<script>
+	let firstName = "Ives";
+	let lastName = "MCgives";
+	let color = "blue"
+	let showText = false
+	let users = [
+		{
+			id:1,
+			name: 'bill'
+		},
+		{
+			id:2,
+			name: 'john'
+		},
+		{
+			id:3,
+			name: 'gracy'
+		},
+	]
+
+
+	$: name = firstName + " " + lastName
+	const toggle = ()=>{
+		color = color ==="blue" ? 'red': "blue"
+		showText = !showText
+		users = [...users, {id: users.length + 1, name: 'new username' }]
+	}
+</script>
+
+<main>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	{#if showText}
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>no text</p>
+	{/if}
+
+	<button style="height: 50px" on:click={toggle}>click</button>
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
