5e22bb49af9d9b414e499696fdd60449dac75f1d
Ivor Lewis <ivor.lewis@gmail.com>
2024-03-04 21:12:00 -0700

    add rating buttons and validation at submit

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,8 +1,11 @@
 <script>
+    import { v4 as uuidv4 } from "uuid";
   import Button from "./Button.svelte";
   import Card from "./Card.svelte";
+  import RatingSelect from "./RatingSelect.svelte";
 
     let text = ''
+    let rating = 10
     let btnDisabled = true
     let min = 11
     let message 
@@ -16,13 +19,26 @@
         btnDisabled = false
         }
     }
+
+    const handleSelect = e => rating = e.detail
+     const handleSubmit = ()=>{
+        if(text.trim().length > min){
+            const newFeedback = {
+                id: uuidv4(),
+                text,
+                rating: +rating
+            }
+            console.log(newFeedback)
+        }
+     }
 </script>
 
 <Card>
   <header>
     <h2>How would you rate your service with us?</h2>
   </header>
-<form >
+<form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect on:rating-select={handleSelect}/>
   <div class="input-group">
     <input type="text" on:input = {handleInput} bind:value = {text} placeholder="Tell us something that keeps you coming back">
     <Button disabled={btnDisabled} type="submit" >Send</Button>
